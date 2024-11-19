15e05923248d3ee1f37cf11a808adb569a86cc60
wmayala <wma.dev5@gmail.com>
2024-11-18 19:59:53 -0600

    Adecuación al equipo HP

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -32,7 +32,7 @@ new class extends Component
 
                 <!-- Navigation Links
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('home')" :active="request()->routeIs('dashboard')" wire:navigate>
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')" wire:navigate>
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>-->
